fix: add rest of files necessary for connected components and masking updates

src/cellmap_analyze/util/image_data_interface.py
@@ -6,28 +6,39 @@ from funlib.geometry import Roi
 
 from cellmap_analyze.util.io_util import split_dataset_path
 from funlib.persistence import open_ds
+from skimage.measure import block_reduce
 
 # Much below taken from flyemflows: https://github.com/janelia-flyem/flyemflows/blob/master/flyemflows/util/util.py
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def open_ds_tensorstore(dataset_path: str, mode="r"):
+def open_ds_tensorstore(dataset_path: str, mode="r", concurrency_limit=None):
     # open with zarr or n5 depending on extension
     filetype = (
         "zarr" if dataset_path.rfind(".zarr") > dataset_path.rfind(".n5") else "n5"
     )
-    spec = {
-        "driver": filetype,
-        # "context": {
-        #     "data_copy_concurrency": {"limit": 1},
-        #     "file_io_concurrency": {"limit": 1},
-        # },
-        "kvstore": {
-            "driver": "file",
-            "path": dataset_path,
-        },
-    }
+    if concurrency_limit:
+        spec = {
+            "driver": filetype,
+            "context": {
+                "data_copy_concurrency": {"limit": concurrency_limit},
+                "file_io_concurrency": {"limit": concurrency_limit},
+            },
+            "kvstore": {
+                "driver": "file",
+                "path": dataset_path,
+            },
+        }
+    else:
+        spec = {
+            "driver": filetype,
+            "kvstore": {
+                "driver": "file",
+                "path": dataset_path,
+            },
+        }
+
     if mode == "r":
         dataset_future = ts.open(spec, read=True, write=False)
     else:
@@ -43,6 +54,7 @@ def to_ndarray_tensorstore(
     offset=None,
     output_voxel_size=None,
     swap_axes=False,
+    custom_fill_value=None,
 ):
     """Read a region of a tensorstore dataset and return it as a numpy array
 
@@ -61,7 +73,8 @@ def to_ndarray_tensorstore(
             offset = Coordinate(offset[::-1])
 
     if roi is None:
-        return dataset.read().result()
+        with ts.Transaction() as txn:
+            return dataset.with_transaction(txn).read().result()
 
     if offset is None:
         offset = Coordinate(np.zeros(roi.dims, dtype=int))
@@ -97,36 +110,72 @@ def to_ndarray_tensorstore(
     )
 
     # Create an array to hold the requested data, filled with a default value (e.g., zeros)
-    output_shape = [s.stop - s.start for s in roi_slices]
+    # output_shape = [s.stop - s.start for s in roi_slices]
 
     if not dataset.fill_value:
         fill_value = 0
-    padded_data = np.ones(output_shape, dtype=dataset.dtype.numpy_dtype) * fill_value
-    padded_slices = tuple(
-        slice(valid_slice.start - s.start, valid_slice.stop - s.start)
+    if custom_fill_value:
+        fill_value = custom_fill_value
+    with ts.Transaction() as txn:
+        data = dataset.with_transaction(txn)[valid_slices].read().result()
+    pad_width = [
+        [valid_slice.start - s.start, s.stop - valid_slice.stop]
         for s, valid_slice in zip(roi_slices, valid_slices)
-    )
+    ]
+    if np.any(np.array(pad_width)):
+        if fill_value == "edge":
+            data = np.pad(
+                data,
+                pad_width=pad_width,
+                mode="edge",
+            )
+        else:
+            data = np.pad(
+                data,
+                pad_width=pad_width,
+                mode="constant",
+                constant_values=fill_value,
+            )
+    # else:
+    #     padded_data = (
+    #         np.ones(output_shape, dtype=dataset.dtype.numpy_dtype) * fill_value
+    #     )
+    #     padded_slices = tuple(
+    #         slice(valid_slice.start - s.start, valid_slice.stop - s.start)
+    #         for s, valid_slice in zip(roi_slices, valid_slices)
+    #     )
 
-    # Read the region of interest from the dataset
-    padded_data[padded_slices] = dataset[valid_slices].read().result()
+    #     # Read the region of interest from the dataset
+    #     padded_data[padded_slices] = dataset[valid_slices].read().result()
 
     if rescale_factor > 1:
         rescale_factor = voxel_size[0] / output_voxel_size[0]
-        padded_data = (
-            padded_data.repeat(rescale_factor, 0)
+        data = (
+            data.repeat(rescale_factor, 0)
             .repeat(rescale_factor, 1)
             .repeat(rescale_factor, 2)
         )
-        padded_data = padded_data[snapped_slices]
+        data = data[snapped_slices]
+
+    elif rescale_factor < 1:
+        data = block_reduce(data, block_size=int(1 / rescale_factor), func=np.median)
+        data = data[snapped_slices]
 
     if swap_axes:
-        padded_data = np.swapaxes(padded_data, 0, 2)
+        data = np.swapaxes(data, 0, 2)
 
-    return padded_data
+    return data
 
 
 class ImageDataInterface:
-    def __init__(self, dataset_path, mode="r", output_voxel_size=None):
+    def __init__(
+        self,
+        dataset_path,
+        mode="r",
+        output_voxel_size=None,
+        custom_fill_value=None,
+        concurrency_limit=1,
+    ):
         self.path = dataset_path
         filename, dataset = split_dataset_path(dataset_path)
         self.ds = open_ds(filename, dataset, mode=mode)
@@ -139,6 +188,8 @@ class ImageDataInterface:
         self.chunk_shape = self.ds.chunk_shape
         self.roi = self.ds.roi
         self.offset = self.ds.roi.offset
+        self.custom_fill_value = custom_fill_value
+        self.concurrency_limit = concurrency_limit
         if output_voxel_size is not None:
             self.output_voxel_size = output_voxel_size
         else:
@@ -146,16 +197,20 @@ class ImageDataInterface:
 
     def to_ndarray_ts(self, roi=None):
         if not self.ts:
-            self.ts = open_ds_tensorstore(self.path)
-
-        return to_ndarray_tensorstore(
+            self.ts = open_ds_tensorstore(
+                self.path, concurrency_limit=self.concurrency_limit
+            )
+        res = to_ndarray_tensorstore(
             self.ts,
             roi,
             self.voxel_size,
             self.offset,
             self.output_voxel_size,
             self.swap_axes,
+            self.custom_fill_value,
         )
+        self.ts = None
+        return res
 
     def to_ndarray_ds(self, roi=None):
         return self.ds.to_ndarray(roi)

src/cellmap_analyze/util/visualization_util.py
@@ -8,16 +8,21 @@ def view_in_neuroglancer(**kwargs):
     viewer = neuroglancer.Viewer()
     with viewer.txn() as s:
         for array_name, array in kwargs.items():
-            if array.dtype not in (float, np.float32):
-                s.layers[array_name] = neuroglancer.SegmentationLayer(
-                    source=neuroglancer.LocalVolume(
-                        data=array,
-                    ),
-                )
-            else:
+            if (
+                array.dtype in (float, np.float32)
+                or "raw" in array_name
+                or "__img" in array_name
+            ):
                 s.layers[array_name] = neuroglancer.ImageLayer(
                     source=neuroglancer.LocalVolume(
                         data=array,
                     ),
                 )
+            else:
+                s.layers[array_name] = neuroglancer.SegmentationLayer(
+                    source=neuroglancer.LocalVolume(
+                        data=array,
+                    ),
+                )
+
     print(viewer.get_viewer_url())

tests/conftest.py
@@ -4,6 +4,7 @@ from funlib.geometry import Roi
 from skimage import measure
 from cellmap_analyze.util.zarr_util import create_multiscale_dataset
 import os
+from scipy import ndimage
 
 # pytest_plugins = [
 #     "fixtures.measure",
@@ -18,6 +19,53 @@ def image_shape():
 @pytest.fixture(scope="session")
 def chunk_size():
     return np.array((4, 4, 4))
+
+
+@pytest.fixture(scope="session")
+def image_with_holes(image_shape):
+    seg = np.zeros(image_shape, dtype=np.uint64)
+
+    # corner cube
+    seg[:3, :3, :3] = 1
+    seg[0, 0, 0] = 0  # shouldnt be filled since it is on border
+    seg[1, 1, 1] = 0  # should be filled
+
+    # hole spanning chunk
+    seg[3:7, 3:7, 3:7] = 2
+    seg[4:6, 4:6, 4:6] = 0
+
+    # two rectangles on top of eachother
+    seg[7:11, 7:11, 7:9] = 3
+    seg[7:11, 7:11, 9:11] = 4
+    seg[8:10, 8:10, 8:10] = 0  # hole between objects should be kept as a hole
+
+    return seg
+
+
+@pytest.fixture(scope="session")
+def mask_one(image_shape):
+    mask = np.zeros(image_shape, dtype=np.uint64)
+    mask[:3, :3, :3] = 1
+    mask[4:6, 2:3, 4:6] = 2
+    return mask
+
+
+@pytest.fixture(scope="session")
+def mask_two(image_shape):
+    mask = np.zeros(image_shape, dtype=np.uint64)
+    mask[7:11, 7:11, 7:9] = 1
+    mask[7:11, 7:11, 9:11] = 2
+    return mask
+
+
+@pytest.fixture(scope="session")
+def image_with_holes_filled(image_with_holes):
+    filled = np.zeros_like(image_with_holes)
+    for id in np.unique(image_with_holes[image_with_holes > 0]):
+        filled += (
+            ndimage.binary_fill_holes(image_with_holes == id).astype(np.uint64)
+        ) * id
+    return filled
 
 
 @pytest.fixture(scope="session")
@@ -56,7 +104,7 @@ def intensity_image(image_shape, chunk_size):
 
 @pytest.fixture(scope="session")
 def connected_components(intensity_image):
-    seg = measure.label(intensity_image > 0)
+    seg = measure.label(intensity_image > 0, connectivity=1)
     return seg
 
 
@@ -264,6 +312,10 @@ def tmp_zarr(tmpdir_factory):
 def test_image_dict(
     blockwise_connected_components,
     intensity_image,
+    image_with_holes,
+    image_with_holes_filled,
+    mask_one,
+    mask_two,
     segmentation_1,
     segmentation_2,
     segmentation_1_downsampled,
@@ -274,6 +326,10 @@ def test_image_dict(
     dict = {
         "blockwise_connected_components": blockwise_connected_components,
         "intensity_image": intensity_image,
+        "image_with_holes": image_with_holes,
+        "image_with_holes_filled": image_with_holes_filled,
+        "mask_one": mask_one,
+        "mask_two": mask_two,
         "segmentation_1": segmentation_1,
         "segmentation_1_downsampled": segmentation_1_downsampled,
         "segmentation_2": segmentation_2,
@@ -304,3 +360,37 @@ def write_zarrs(tmp_zarr, test_image_dict, voxel_size, chunk_size):
             write_size=chunk_size * current_voxel_size,
         )
         ds.data[:] = data
+
+
+# %%
+# import numpy as np
+# from scipy import ndimage
+
+# seg = np.zeros((11, 11, 11), dtype=np.uint64)
+
+# # corner cube
+# seg[:3, :3, :3] = 1
+# seg[0, 0, 0] = 0  # shouldnt be filled since it is on border
+# seg[1, 1, 1] = 0  # should be filled
+
+# # hole spanning chunk
+# seg[3:7, 3:7, 3:7] = 2
+# seg[4:6, 4:6, 4:6] = 0
+
+# # two rectangles on top of eachother
+# seg[7:11, 7:11, 7:9] = 3
+# seg[7:11, 7:11, 9:11] = 4
+# seg[8:10, 8:10, 8:10] = 0  # hole between objects should be kept as a hole
+
+# filled = np.zeros_like(seg)
+# for id in np.unique(seg[seg > 0]):
+#     filled += (ndimage.binary_fill_holes(seg == id).astype(np.uint64)) * id
+#     print(np.amax(filled))
+# import cellmap_analyze.util.visualization_util
+# from importlib import reload
+
+# reload(cellmap_analyze.util.visualization_util)
+# from cellmap_analyze.util.visualization_util import view_in_neuroglancer
+
+# view_in_neuroglancer(seg=seg, filled=filled)
+# %%