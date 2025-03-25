fix: :bug: fix hole datatype so we don't have overflow issues

src/cellmap_analyze/process/fill_holes.py
@@ -96,12 +96,12 @@ class FillHoles:
         holes = holes_idi.to_ndarray_ts(roi)
         input = input_idi.to_ndarray_ts(roi)
 
-        max_input_id = np.max(input)
-        holes[holes > 0] = holes[holes > 0] + max_input_id  # so there is no id overlap
-
         input_boundaries = find_boundaries(input, mode="inner").astype(np.uint64)
         hole_boundaries = find_boundaries(holes, mode="inner").astype(np.uint64)
 
+        max_input_id = np.max(input)
+        holes = holes.astype(np.uint64)
+        holes[holes > 0] += max_input_id  # so there is no id overlap
         data = holes + input
         mask = np.logical_or(input_boundaries, hole_boundaries)
         touching_ids = ConnectedComponents.get_touching_ids(data, mask, connectivity)
@@ -112,7 +112,7 @@ class FillHoles:
             id2 -= max_input_id
             # then input objects are touching holes
             object_ids = block.hole_to_object_dict.get(id2, set())
-            objectsIds = object_ids.add(id1)
+            object_ids.add(id1)
             block.hole_to_object_dict[id2] = object_ids
 
         return block
@@ -209,7 +209,7 @@ class FillHoles:
 
         if len(block.relabeling_dict) > 0:
             # couldnt do inplace for large uint types because it was converting to floats
-            relabeled = np.zeros_like(holes)
+            relabeled = np.zeros_like(input)
             keys, values = zip(*block.relabeling_dict.items())
             replace_values(holes, list(keys), list(values), out_array=relabeled)
             holes = relabeled
@@ -304,4 +304,43 @@ class FillHoles:
 # from skimage.segmentation import find_boundaries
 
 # find_boundaries(labels, mode="inner").astype(np.uint8)
-# # %%
+# %%
+# from funlib.geometry import Roi
+# from cellmap_analyze.util.image_data_interface import ImageDataInterface
+# import numpy as np
+# from skimage.segmentation import find_boundaries
+# from cellmap_analyze.process.connected_components import ConnectedComponents
+
+# connectivity = 1
+# holes_idi = ImageDataInterface(
+#     "/nrs/cellmap/ackermand/cellmap/c-elegans/jrc_c-elegans-comma-1/jrc_c-elegans-comma-1.zarr/lyso_filled_holes/s0"
+# )
+# input_idi = ImageDataInterface(
+#     "/nrs/cellmap/ackermand/cellmap/c-elegans/jrc_c-elegans-comma-1/jrc_c-elegans-comma-1.zarr/lyso/s0"
+# )
+# roi = Roi((39342, 8715, 19275), [212 * 16] * 3)
+# holes = holes_idi.to_ndarray_ts(roi)
+# input = input_idi.to_ndarray_ts(roi)
+
+# max_input_id = np.max(input)
+
+# input_boundaries = find_boundaries(input, mode="inner").astype(np.uint64)
+# hole_boundaries = find_boundaries(holes, mode="inner").astype(np.uint64)
+
+# holes = holes.astype(np.uint64)
+# holes[holes > 0] += max_input_id  # so there is no id overlap
+# data = holes + input
+# mask = np.logical_or(input_boundaries, hole_boundaries)
+# touching_ids = ConnectedComponents.get_touching_ids(data, mask, connectivity)
+
+# hole_to_object_dict = {}
+# for id1, id2 in touching_ids:
+#     if id2 <= max_input_id:
+#         continue
+#     id2 -= max_input_id
+#     # then input objects are touching holes
+#     object_ids = hole_to_object_dict.get(id2, set())
+#     object_ids.add(id1)
+#     hole_to_object_dict[id2] = object_ids
+
+# %%