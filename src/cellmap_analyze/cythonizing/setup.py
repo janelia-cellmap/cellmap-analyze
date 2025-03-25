620323e4287e1ef233e595427cc4e067f79480fe
David Ackerman <ackermand@janelia.hhmi.org>
2025-03-25 15:46:26 -0400

    update setup.py

--- src/cellmap_analyze/cythonizing/setup.py
+++ src/cellmap_analyze/cythonizing/setup.py
@@ -1,7 +1,22 @@
-from setuptools import setup
-from Cython.Build import cythonize
+# # from setuptools import setup
+# # from Cython.Build import cythonize
 
-setup(
-    ext_modules=cythonize(["bresenham3D.pyx", "process_arrays.pyx"]),
-    # extra_compile_args=["-O3", "-std=c11"],
-)
+# # setup(
+# #     ext_modules=cythonize(["bresenham3D.pyx", "process_arrays.pyx"]),
+# #     # extra_compile_args=["-O3", "-std=c11"],
+# # )
+# from setuptools import setup, Extension
+# from Cython.Build import cythonize
+
+# extensions = [
+#     Extension("cythonizing.bresenham3D", ["cythonizing/bresenham3D.pyx"]),
+#     Extension("cythonizing.process_arrays", ["cythonizing/process_arrays.pyx"]),
+# ]
+
+# setup(
+#     name="cythonizing",
+#     version="0.1",
+#     packages=["cythonizing"],
+#     ext_modules=cythonize(extensions),
+#     zip_safe=False,
+# )
