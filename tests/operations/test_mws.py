450d2ff52ac52778bb44ed62f329ff1cf1084f7d
David Ackerman <ackermand@janelia.hhmi.org>
2025-05-21 12:23:58 -0400

    remove looping

--- tests/operations/test_mws.py
+++ tests/operations/test_mws.py
@@ -1,30 +1,37 @@
+import pytest
 from cellmap_analyze.process.mutex_watershed import MutexWatershed
+import fastmorph
 import fastremap
-import numpy as np
-
 from cellmap_analyze.util.image_data_interface import (
     ImageDataInterface,
 )
 from tests.test_utils import arrays_equal_up_to_id_ordering
 
 
-def test_connected_components(
-    tmp_zarr,
-    segmentation_cylinders,
-):
+@pytest.mark.parametrize("do_opening", [True, False])
+def test_mutex_watershed(tmp_zarr, segmentation_cylinders, do_opening):
 
     mw = MutexWatershed(
         affinities_path=f"{tmp_zarr}/affinities_cylinders",
-        output_path=f"{tmp_zarr}/test_mws",
+        output_path=f"{tmp_zarr}/test_mws_{do_opening}",
         adjacent_edge_bias=0,
         lr_bias_ratio=0,
         filter_val=0.05,
         connectivity=3,
         num_workers=1,
         delete_tmp=True,
+        do_opening=do_opening,
     )
     mw.get_connected_components()
-    test_data = ImageDataInterface(f"{tmp_zarr}/test_mws/s0").to_ndarray_ts()
+    test_data = ImageDataInterface(
+        f"{tmp_zarr}/test_mws_{do_opening}/s0"
+    ).to_ndarray_ts()
     ground_truth = segmentation_cylinders.copy()
+
+    if do_opening:
+        ground_truth = fastmorph.erode(ground_truth)
+        ground_truth = fastmorph.dilate(ground_truth)
+        fastremap.renumber(ground_truth, in_place=True)
+
     ground_truth[0, 0, 0] = 0  # since we set it to nonzero in its generation
     assert arrays_equal_up_to_id_ordering(test_data, ground_truth)
